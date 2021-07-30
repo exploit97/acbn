3db981b021c3c092c9a2a7a2e17ffcb326f33617
exploit <yvindjhonnelmahoukou@gmail.com>
2021-07-30 19:13:54 +0100

    second commit

--- blog/models.py
+++ blog/models.py
@@ -73,12 +73,12 @@ class Documents(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = RichTextField(blank=True, null=True)
-    doc_file = models.FileField(blank=True, null=True, upload_to='files/')
+    doc_file = models.FileField(upload_to='files/')
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now= True)    
 
     def __str__(self):
-        return self.post.title
+        return self.title
     
     def get_absolute_url(self):
         return reverse("blogs:documents")
