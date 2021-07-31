82c6bd486e80f0ebc7dd32c571fa8f4e35bcd966
exploit <yvindjhonnelmahoukou@gmail.com>
2021-07-31 14:08:23 +0100

    push deployement1

--- acbn/settings.py
+++ acbn/settings.py
@@ -46,8 +46,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 else:
     DEBUG = True
