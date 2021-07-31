3c84647c9d2d29179fa5d0efb158b46d14f8588e
exploit <yvindjhonnelmahoukou@gmail.com>
2021-07-31 11:27:17 +0100

    push deployement

--- acbn/settings.py
+++ acbn/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3vcj0tfceu!%#fyvk8mr^(0nn0#$5@114h4d#d*0e76v@k8fbd' # development key for the moment
+SECRET_KEY = config('SECRET_KEY') # development key for the moment
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 
 if os.environ.get('ENV') == 'PRODUCTION':
@@ -110,11 +110,11 @@ WSGI_APPLICATION = 'acbn.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'acbn',
-        'USER': 'exploit',
-        'PASSWORD': '19041997',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('PORT'),
     }
 }
 
